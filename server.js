66becbd648e29bc176583746ae2bbcfb22a2e663
Austxgolfer <taf442001@gmail.com>
2019-07-27 20:12:38 -0500

    updating server and json

--- server.js
+++ server.js
@@ -1,78 +1,31 @@
 const express = require("express");
-const path = require("path");
-const logger = require("morgan");
+
 const mongoose = require("mongoose");
-
-const axios = require("axios");
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/GBooks";
-
-const db = require("./models");
-
-const PORT = process.env.PORT || 3000;
+const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Define middleware here
-app.use(logger("dev"));
+// Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-app.use(express.static("public"));
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// Add routes, both API and view
+app.use(routes);
 
-app.get("/api/books", function(req, res) {
-  db.Book.find({})
-    .then(function(dbBook) {
-      res.json(dbBook);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-app.post("/api/books", function(req, res) {
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      console.log("updatenote", dbNote);
-      return db.Book.findOneAndUpdate(
-        { _id: req.params.id },
-        { note: dbNote._id },
-        { new: true }
-      );
-    })
-    .then(function(dbBook) {
-      res.json(dbBook);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-app.post("/api/books/:id", function(req, res) {
-  db.Note.deleteOne({ _id: req.body.id })
-    .then(function() {
-      db.Note.findOne({ artid: req.body.artid }).then(function(dbNote) {
-        console.log("updatenote", dbNote);
-        return db.Book.findOneAndUpdate(
-          { _id: req.body.artid },
-          { note: dbNote._id },
-          { new: true }
-        );
-      });
-    })
-    .then(function(dbBook) {
-      res.json(dbBook);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
-// Define API routes here
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
